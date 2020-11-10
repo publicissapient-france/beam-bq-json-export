0e77255e88319be3abe52f37a31cfb2afd86c580
Vincent Villet <vincentvillet@MacBook-Pro-de-Vincent.local>
2020-11-10 11:08:21 +0100

    dataflow export working

--- export_query.sql
+++ export_query.sql
@@ -8,6 +8,6 @@ SELECT
   start_date,
   start_station_id,
   start_station_name,
-  REGEXP_REPLACE(CONCAT(DATE(CURRENT_TIMESTAMP),"/", start_station_id, FORMAT("_%03d", CAST(ROW_NUMBER() OVER(PARTITION BY start_station_id ORDER BY start_date)/15000 AS INT64))), "/", "-") AS file_name
+  CONCAT(DATE(CURRENT_TIMESTAMP),"/", start_station_id, FORMAT("_%03d", CAST(ROW_NUMBER() OVER(PARTITION BY start_station_id ORDER BY start_date)/15000 AS INT64))) AS file_name
 FROM
   `bigquery-public-data.london_bicycles.cycle_hire`
--- main.sh
+++ main.sh
@@ -2,29 +2,23 @@ EXPORT_DATASET="london_cycles"
 EXPORT_BUCKET="beam-bq-json-export"
 PROJECT_ID="${EXPORT_BUCKET}"
 EXPORT_TABLE="london_cycles_export"
+export GOOGLE_APPLICATION_CREDENTIALS="service_account_key.json"
 
 bq --location="EU" mk --dataset $PROJECT_ID:$EXPORT_DATASET
 
 QUERY="$(cat ./export_query.sql)";
 bq query --replace -n 0 --nouse_legacy_sql --nouse_cache --nobatch --allow_large_results --quiet \
---destination_table "${EXPORT_DATASET}.${EXPORT_TABLE}" "${QUERY}"
-
-sudo apt update
-yes Y | sudo apt install python3-pip
-sudo pip3 install virtualenv
+--destination_table "${PROJECT_ID}:${EXPORT_DATASET}.${EXPORT_TABLE}" "${QUERY}"
 
 #Dataflow requires python 3.7
 virtualenv -p python3 venv
 
 source venv/bin/activate
-pip3 install apache-beam[gcp]
+# Use quotation marks for zsh mac
+pip3 install "apache-beam[gcp]"
 
-echo -e "$(date -u) Make valid json export \n------------------"
-source $ROOT_DIR/venv/bin/activate
-python $SCRIPTS_DIR/valid_json_bq_export_to_mdm.py --project=fnacdarty-products --region=europe-west1 --runner=DataFlow \
---input= \
+python ./beam_bq_json_export.py --project="${PROJECT_ID}" --region=europe-west1 --runner=DataFlow \
+--input="${PROJECT_ID}:${EXPORT_DATASET}.${EXPORT_TABLE}" \
 --staging_location=gs://$EXPORT_BUCKET/test --temp_location gs://$EXPORT_BUCKET/test \
 --output=gs://$EXPORT_BUCKET --save_main_session True
-# Important to deactivate venv not to run into issues with update_view script that calls does_it_exist_in_big_query function
-# that uses python2 with print (syntax error if default python is python3, which is the case within the virtualenv).
 deactivate
